7a3c30ebe66511045a9202d753bf22969bcb897c
Jake <68775263+jb0s@users.noreply.github.com>
2023-11-03 17:24:33 +0100

    Commence the Java

---
+++ src/main/java/dev/jb0s/clockgame/ClockgamePlugin.java
@@ -0,0 +1,17 @@
+package dev.jb0s.clockgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClockgamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
